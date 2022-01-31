a97d2ebf36928ee255d7ca6c9ba0bcfc4e01b3c1
WhysoSarthak <sarthakchoudhary507@gmail.com>
2022-02-01 03:29:43 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+let weather = {
+    "apiKey":"2f43fff68481a0f36734981eb3fe4ec5",
+    fetchWeather:function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city 
+        + "&units=metric&appid="
+        + this.apiKey
+    )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const {name} = data;
+        const {icon,description} = data.weather[0];
+        const {temp, humidity, feels_like} = data.main;
+        const {speed} = data.wind;
+
+        console.log(name , icon, description, temp, humidity, feels_like, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText= temp + "°C";
+        document.querySelector(".feels_like").innerText= "Feels like: " + feels_like + "°C";
+        document.querySelector(".humidity").innerText="Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText="Wind speed : " + speed + "Km/h";
+
+    } 
+};
