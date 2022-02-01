e4f114c313246967352de3ec8f81e43e92de2388
WhysoSarthak <sarthakchoudhary507@gmail.com>
2022-02-01 23:12:26 +0530

    final commit

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ let weather = {
         const {temp, humidity, feels_like} = data.main;
         const {speed} = data.wind;
 
-        console.log(name , icon, description, temp, humidity, feels_like, speed);
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
         document.querySelector(".description").innerText = description;
@@ -23,6 +22,22 @@ let weather = {
         document.querySelector(".feels_like").innerText= "Feels like: " + feels_like + "°C";
         document.querySelector(".humidity").innerText="Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText="Wind speed : " + speed + "Km/h";
-
-    } 
+        document.querySelector(".weather").classList.remove("loading")
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+    },
+    search :function() {
+        this.fetchWeather(document.querySelector(".search-bar").value)
+    }
 };
+
+document.querySelector(".search button").addEventListener("click",function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key=="Enter") {
+        weather.search();        
+    }
+});
+
+weather.fetchWeather("Noida");
