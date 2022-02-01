37eca0778931876cb60cfccc73a9bfe987101436
WhysoSarthak <sarthakchoudhary507@gmail.com>
2022-02-02 00:58:11 +0530

    Cosmetic Changes

--- script.js
+++ script.js
@@ -11,19 +11,20 @@ let weather = {
     },
     displayWeather: function(data) {
         const {name} = data;
-        const {icon,main} = data.weather[0];
+        const {icon, main , description} = data.weather[0];
         const {temp, humidity, feels_like} = data.main;
         const {speed} = data.wind;
+        let x = Math.round(temp);
 
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+ icon +"@2x.png";
         document.querySelector(".main").innerText = main;
-        document.querySelector(".temp").innerText= temp + "°C";
-        document.querySelector(".feels_like").innerText= "Feels like: " + feels_like + "°C";
+        document.querySelector(".temp").innerText= x + " °C";
+        document.querySelector(".feels_like").innerText= "Feels like: " + feels_like + " °C";
         document.querySelector(".humidity").innerText="Humidity: " + humidity + "%";
-        document.querySelector(".wind").innerText="Wind speed : " + speed + "m/s";
+        document.querySelector(".wind").innerText="Wind speed : " + speed + " m/s";
         document.querySelector(".weather").classList.remove("loading")
-        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + main + "')"
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + description + "')"
     },
     search :function() {
         this.fetchWeather(document.querySelector(".search-bar").value)
